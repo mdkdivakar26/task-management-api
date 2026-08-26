4b36e27cf163926b3118c819359173961f1e176f
Divakar M <mdkdivakar26@gmail.com>
2026-08-26 18:56:02 +0530

    Restore Task.java

---
+++ src/main/java/com/divakar/taskmanagement/model/Task.java
@@ -0,0 +1,68 @@
+package com.divakar.taskmanagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Title is required")
+	@Size(max = 100, message = "Title must not exceed 100 characters")
+	private String title;
+
+	@Size(max = 500, message = "Description must not exceed 500 characters")
+	private String description;
+
+    private boolean completed;
+
+    public Task() {
+    }
+
+    public Task(Long id, String title, String description, boolean completed) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
